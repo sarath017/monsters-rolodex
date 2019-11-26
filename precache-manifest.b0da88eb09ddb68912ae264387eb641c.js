53bc8fd542b4a640227866467b5c7863f7152cb6
Sarath Kumar K <sarathkumar_k@CHMB113708.local>
2019-11-26 17:50:47 +0530

    Updates

---
+++ precache-manifest.b0da88eb09ddb68912ae264387eb641c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ea1b2a2c6875b51e595e8066ead5be",
+    "url": "/monsters-roloex/index.html"
+  },
+  {
+    "revision": "93fde2a08c4f2ae66c7a",
+    "url": "/monsters-roloex/static/css/main.b835bec0.chunk.css"
+  },
+  {
+    "revision": "7573cdd6c3c3ce41db3b",
+    "url": "/monsters-roloex/static/js/2.cf4849f8.chunk.js"
+  },
+  {
+    "revision": "93fde2a08c4f2ae66c7a",
+    "url": "/monsters-roloex/static/js/main.7dab429a.chunk.js"
+  },
+  {
+    "revision": "3a480fdac28181000ef2",
+    "url": "/monsters-roloex/static/js/runtime-main.c8a88bbc.js"
+  }
+]);
